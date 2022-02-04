3e316581b6fd215f6b87fb75b9fbb8421509b326
Aravind <aravind.narayanan@students.iiit.ac.in>
2022-02-05 03:57:14 +0530

    I/O tuts

---
+++ tutorials/filtering/tut1/CMakeFiles/PassThroughFilter.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PassThroughFilter.dir/PassThroughFilter.cpp.o"
+  "PassThroughFilter.pdb"
+  "PassThroughFilter"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PassThroughFilter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
